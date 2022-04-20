4e2c211530d384e16068db3b9ecad86dc9910d85
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2022-04-20 23:23:47 +0300

    Update registration.php

--- 4.20.2022 (PHP & MySql Registeration)/registration.php
+++ 4.20.2022 (PHP & MySql Registeration)/registration.php
@@ -2,21 +2,21 @@
 
 session_start();
 
-$con = mysqli_connect('localhost','blabla_admin1','Password1231231234124324');
-mysqli_select_db($con, 'webswptt_register');
+$con = mysqli_connect('localhost','adminName','password123');
+mysqli_select_db($con, 'register');
 $name = $_POST['user'];
 $pass = $_POST['password'];
-$query = "select * from registertable where name = '$name' ";
+$query = "select * from userTable where name = '$name' ";
 $result = mysqli_query($con,$query);
 $num = mysqli_num_rows($result);
 
 if($num==1){
-    echo "username taken.";
+    echo "username already taken.";
 }
 
 else{
-    $reg="insert into registertable(name, password) values ('$name','$pass')";
-    mysqli_query($con,$reg);
+    $register="insert into registertable(name, password) values ('$name','$pass')";
+    mysqli_query($con,$register);
     echo "registered successfully";
     
 }
